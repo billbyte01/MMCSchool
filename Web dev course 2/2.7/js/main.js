194e9a941d0df324a66c0c792647f140aa6f44dc
billbyte <bill.byte@pm.me>
2024-10-27 08:51:13 +0100

    variables rules

--- Web dev course 2/2.7/js/main.js
+++ Web dev course 2/2.7/js/main.js
@@ -3,3 +3,11 @@ const food = 'Bigos'
 const age = 24
 
 console.log(`Hello, I am ${name}, love to eat ${food} and have ${age} years.`);
+
+const favColor = 'red' //we use camel case
+// const 1favtest = 'red' - we do not add numbers 
+// const fav-test = 'red' - we do not use '-'
+// const fav_test = 'red' - it is ok but we use camel case
+// const User = 'red' - reserved for classes
+// const USER = 'red' - for most important variables
+// reserved names - not for variables: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar#reserved_words
