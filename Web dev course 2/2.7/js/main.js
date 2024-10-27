915fa5cc7f497201750098591a21825f6c6d38c2
billbyte <bill.byte@pm.me>
2024-10-27 08:52:02 +0100

    up

--- Web dev course 2/2.7/js/main.js
+++ Web dev course 2/2.7/js/main.js
@@ -4,10 +4,10 @@ const age = 24
 
 console.log(`Hello, I am ${name}, love to eat ${food} and have ${age} years.`);
 
-const favColor = 'red' //we use camel case
+const favColor = 'red' //we use camelCase + eng language
 // const 1favtest = 'red' - we do not add numbers 
 // const fav-test = 'red' - we do not use '-'
-// const fav_test = 'red' - it is ok but we use camel case
+// const fav_test = 'red' - it is ok but we use camelCase
 // const User = 'red' - reserved for classes
 // const USER = 'red' - for most important variables
 // reserved names - not for variables: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar#reserved_words
