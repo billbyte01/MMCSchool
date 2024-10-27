09dd704bd72e2f0015015d03412f1f76760b296a
billbyte <bill.byte@pm.me>
2024-10-27 08:54:08 +0100

    up

--- Web dev course 2/2.7/js/main.js
+++ Web dev course 2/2.7/js/main.js
@@ -5,7 +5,7 @@ const age = 24
 console.log(`Hello, I am ${name}, love to eat ${food} and have ${age} years.`);
 
 const favColor = 'red' //we use camelCase + eng language
-// const 1favtest = 'red' - we do not add numbers 
+// const 1favtest = 'red' - we do not add numbers at the beginning
 // const fav-test = 'red' - we do not use '-'
 // const fav_test = 'red' - it is ok but we use camelCase
 // const User = 'red' - reserved for classes
