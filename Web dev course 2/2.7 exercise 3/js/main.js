627912b9e3dd8e912186e9b4cf8c478a2bb452a3
billbyte <bill.byte@pm.me>
2024-11-10 22:48:32 +0100

    up

--- Web dev course 2/2.7 exercise 3/js/main.js
+++ Web dev course 2/2.7 exercise 3/js/main.js
@@ -6,8 +6,25 @@ const p = document.querySelector('p')
 let fontSize = 20
 
 function bigText() {
-	p.style.fontSize = fontSize + 'px'
-    fontSize += fontSize
+	if (fontSize < 60) {
+		fontSize = fontSize + 5
+		p.style.fontSize = fontSize + 'px'
+		console.log(fontSize)
+	}
+}
+
+function smallText() {
+	if (fontSize > 5) {
+		fontSize = fontSize - 5
+		p.style.fontSize = fontSize + 'px'
+		console.log(fontSize)
+	}
+}
+
+function goldColor() {
+	p.style.color = p.style.color === 'gold' ? 'white' : 'gold'
 }
 
 sizeUp.addEventListener('click', bigText)
+sizeDown.addEventListener('click', smallText)
+color.addEventListener('click', goldColor)
