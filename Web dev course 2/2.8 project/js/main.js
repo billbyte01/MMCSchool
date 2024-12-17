182a867865027dc7bad831e28fd075e660d8adaf
billbyte <bill.byte@pm.me>
2024-12-17 23:48:03 +0100

    up

--- Web dev course 2/2.8 project/js/main.js
+++ Web dev course 2/2.8 project/js/main.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
     const navigation = document.querySelector('.navbar')
+    const allNavItems = document.querySelectorAll('.nav-link')
+    const  navList = document.querySelector('.navbar-collapse')
     function addShadow (){
         if (window.scrollY >= 100){
             navigation.classList.add('shadow-bg')
@@ -7,5 +9,10 @@ document.addEventListener('DOMContentLoaded', function(){
             navigation.classList.remove('shadow-bg')
         }
     }
+
+allNavItems.forEach(item => item.addEventListener('click', () => {
+    navList.classList.remove('show')
+}))
+
     window.addEventListener('scroll', addShadow)
 })
