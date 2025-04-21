7ea5b6adf19651ee067b209be2bbee94d62268ee
billbyte <bill.byte@pm.me>
2025-04-21 14:33:07 +0200

    JS methods

--- Web_dev_course_JS/JS.1_basics/js/main.js
+++ Web_dev_course_JS/JS.1_basics/js/main.js
@@ -1,6 +1,48 @@
-var xd1 = 123
-var xd2 = 321
+/*
+const xd1 = 123
+const xd2 = '321'
+console.log(`xd1: ${xd1}`);
+console.log(`xd2: ${xd2}`);
+console.log(xd1 + '' + xd2)
+console.log(`${xd1}${xd2}`) //template string
+console.log(`Type of xd1: ${typeof xd1}`)
+console.log(`Type of xd2: ${typeof xd2}`)
 
-console.log(xd1 + '' + xd2);
+const quote = 'He said: "whymyeyesya"'
+console.log(quote);
+console.log(`Phrase lenght: ${quote.length}`);
+*/
 
-console.log(`${xd1}${xd2}`); //template string
+/*
+ZADANIE z JS.2
+ Metody do wykorzystania:
+ charAt()
+ includes()
+ replace()* podchwytliwe 🙂 
+ slice()
+ split()
+ toLowerCase()
+ toUpperCase()
+*/
+ 
+const text1 = 'powiększ mnie!'
+const text11 = text1.toUpperCase()
+console.log(text11);
+const text2 = 'ZAPISZ MNIE MAŁYMI LITERAMI'
+const text21 = text2.toLowerCase()
+console.log(text21);
+const text3 = '$#%#^ wytnij te dziwne znaki na początku!'
+const text31 = text3.slice(6)
+console.log(text31);
+const text4 = 'sprawdź, czy zawieram słowo "czy"'
+const text41 = text4.includes("czy")
+console.log(text41);
+const text5 = 'wyLoguj w konsoli tylko literę "L", która znajduje się w wyrazie "Wyloguj"'
+const text51 = text5.charAt("2")
+console.log(text51);
+const text6 = 'pies zamień każde słowo pies, na słowo kot pies'
+const text61 = text6.replaceAll("pies", "kot")
+console.log(text61);
+const text7 = 'podziel, ten, string, od, przecinków'
+const text71 = text7.split(',')
+console.log(text71);
