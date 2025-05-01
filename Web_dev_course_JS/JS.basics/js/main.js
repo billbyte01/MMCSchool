fe51f5405bb6c00027a0560fb65f337763a407e6
billbyte <bill.byte@pm.me>
2025-05-01 22:29:06 +0200

    up

--- Web_dev_course_JS/JS.basics/js/main.js
+++ Web_dev_course_JS/JS.basics/js/main.js
@@ -177,6 +177,7 @@ const tableZ = [...tableX, ...tableY]
 console.log(tableZ);
 */
 
+/*
 const numbers = [0, 0, 1, 1, 2, 2, 2]
 const colors = ['red', 'green', 'blue', true, 123]
 const cars = [123, true, 'audi', 'bmw', 'mercedes', 'ferrari', '🤷‍♂️', '👀']
@@ -184,9 +185,10 @@ const numbers2 = numbers.slice(0,2)
 const numbers3 = numbers.slice(-3)
 console.log(numbers2);
 console.log(numbers3);
-randomStuff = colors.splice(-2, 2)
+randomStuff = colors.splice(-2)
 console.log(colors);
 console.log(randomStuff);
 newCars = cars.splice(2,4,'test')
 console.log(cars);
 console.log(newCars);
+*/
