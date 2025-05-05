0eb300bae4193173c199237b6617c9cb48bdcb01
billbyte <bill.byte@pm.me>
2025-05-05 20:11:37 +0200

    append / remove

--- Web_dev_course_JS/JS.medium/js/main.js
+++ Web_dev_course_JS/JS.medium/js/main.js
@@ -2,10 +2,13 @@ const ulList = document.createElement('ul')
 const liItem = document.createElement('li')
 liItem.textContent = 'hello'
 document.body.appendChild(ulList)
-ulList.appendChild(liItem)
+ulList.append(liItem)
 
 const btn = document.querySelector('button')
 // btn.outerHTML = '<li>123</li>'
 // btn.innerHTML = '<li>123</li>'
 // btn.innerText = '<li>123</li>'
 btn.textContent = '<li>123</li>'
+
+// document.body.removeChild(btn)
+btn.remove()
