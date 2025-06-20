ccfb6ff410dff2b28cf2d6fdacaaa0393e1b60d1
billbyte <bill.byte@pm.me>
2025-06-20 17:06:16 +0200

    up

--- Web_dev_course_JS/mini_projects/10.7/js/main.js
+++ Web_dev_course_JS/mini_projects/10.7/js/main.js
@@ -30,4 +30,23 @@ const addNewTodo = () => {
     }
 }
 
+const createToolsArea = () => {
+    let newDiv = document.createElement('div')
+    newDiv.classList.add('tools')
+    let newButton1 = document.createElement('button')
+    newButton1.classList.add('complete')
+    newButton1.innerHTML = '<i class"fas fa-check"></i>'
+    let newButton2 = document.createElement('button')
+    newButton2.classList.add('edit')
+    newButton2.textContent = 'EDIT'
+    let newButton3 = document.createElement('button')
+    newButton3.classList.add('delete')
+    newButton3.innerHTML = '<i class"fas fa-times"></i>'
+    newDiv.append(newButton1)
+    newDiv.append(newButton2)
+    newDiv.append(newButton3)
+    console.log(newDiv);
+}
+createToolsArea()
+
 document.addEventListener('DOMContentLoaded', main)
