9f635062e9d7d12e5c29ec15113103cf0750745d
billbyte <bill.byte@pm.me>
2025-07-25 11:08:52 +0200

    JS.12 weather app project

--- Web_dev_course_JS/JS.12_JSON-AJAX-API/js/main.js
+++ Web_dev_course_JS/JS.12_JSON-AJAX-API/js/main.js
@@ -1,4 +1,4 @@
-const btn = document.querySelector('button')
+// const btn = document.querySelector('button')
 const img = document.querySelector('img')
 
 const URL = 'https://dog.ceo/api/breeds/image/random'
@@ -7,9 +7,21 @@ const URL = 'https://dog.ceo/api/breeds/image/random'
 // .then(res => console.log(res))
 // .catch(err => console.error(err))
 
-btn.addEventListener('click', () => {
+// btn.addEventListener('click', () => {
 	fetch(URL)
 		.then((res) => res.json())
 		.then((data) => img.setAttribute('src', data.message))
 		.catch((err) => console.error(err))
-})
+// })
+
+// using async/await istead of .then/.catch
+async function test() {
+	try {
+		const res = await fetch(URL)
+		const data = await res.json()
+		console.log(data);
+	} catch {
+		console.error('error')
+	}
+}
+test()
