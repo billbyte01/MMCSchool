dcc036b69a66f278e1e5dbb88377235e8e0adac5
billbyte <bill.byte@pm.me>
2025-09-16 23:45:15 +0200

    up

--- Web_dev_course_4/components/header/js/main.js
+++ Web_dev_course_4/components/header/js/main.js
@@ -1,8 +1,25 @@
+// hiding/showing menu on mobile
+
 const burgerLine = document.querySelector('.line')
 const navBurger = document.querySelector('.nav-burger')
 const navMobile = document.querySelector('nav')
 
 navBurger.addEventListener('click', () => {
 	burgerLine.classList.toggle('burger-x')
-    navMobile.classList.toggle('nav-hide')
+	navMobile.classList.toggle('nav-hide')
+})
+
+// hiding/showing header when scrolling
+
+const headerHide = document.querySelector('header')
+let lastScroll = 0
+
+window.addEventListener('scroll', () => {
+	const currentScroll = window.scrollY
+	if (window.scrollY > 80) {
+		headerHide.style.transform = 'translateY(-100%)'
+	} else {
+		headerHide.style.transform = 'translateY(0)'
+	}
+	lastScroll = currentScroll
 })
